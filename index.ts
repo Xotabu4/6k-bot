25e6b2135b446d5354ee352ce7cdad2c2b7ec221
Oleksandr Khotemskyi <xotabu4@gmail.com>
2020-11-19 11:00:37 +0200

    added heroku proc file

--- index.ts
+++ index.ts
@@ -4,16 +4,8 @@ import got from 'got'
 if (!process.env.BOT_TOKEN) {
     throw new Error('BOT_TOKEN env var needed')
 }
-
-console.log(process.env.BOT_TOKEN)
-
 const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.start((ctx) => ctx.reply('Welcome!'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-
-
 // [
 // {"ccy":"USD","base_ccy":"UAH","buy":"28.05000","sale":"28.45000"},
 // {"ccy":"EUR","base_ccy":"UAH","buy":"33.15000","sale":"33.75000"},
@@ -22,11 +14,10 @@ bot.hears('hi', (ctx) => ctx.reply('Hey there'))
 // ]
 bot.hears('/exchangeRate', async (ctx) => {
     const body = JSON.parse((await got('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5')).body)
-
     ctx.reply(`
-       USD покупка: ${body.find(c => c.ccy === 'USD').buy}
-       USD продажа: ${body.find(c => c.ccy === 'USD').sale}
-    `)
+USD покупка: ${body.find(c => c.ccy === 'USD').buy}
+USD продажа: ${body.find(c => c.ccy === 'USD').sale}
+`)
 
 })
 
